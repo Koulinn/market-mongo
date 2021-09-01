7e46df06264d20dd05e211bb3f447962b6fcb1ae
Koulinn <drdverzola@gmail.com>
2021-09-01 20:04:11 +0100

    Initial files

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from "express"
+import cors from "cors"
+import lib from "./src/lib/index.js"
+import mongoose from 'mongoose'
+
+const { errorHandlers, serverConfig } = lib
+
+
+const server = express()
+const { PORT } = process.env
+
+server.use(express.json())
+server.use(cors(serverConfig))
+
+// server.use("/cart", cartRouter)
+
+
+
+
+
+
+server.use(errorHandlers.forbidden)
+server.use(errorHandlers.notFound)
+server.use(errorHandlers.badRequest)
+server.use(errorHandlers.server)
+
+
+
+
+mongoose.connect(process.env.MONGO_CONN)
+mongoose.connection.on('connected', () => {
+  try {
+    console.log('Mongo connected')
+    server.listen(PORT, async () => {
+      console.log("🚀 Server is running on port ", PORT)
+    })
+    mongoose.connection.on('error', error => {
+      console.log('Mongo error: ', error)
+    })
+    server.on("error", (error) =>
+      console.log("🚀 Server is crashed due to ", error)
+    )
+
+  } catch (error) {
+    console.log(error)
+  }
+})
+

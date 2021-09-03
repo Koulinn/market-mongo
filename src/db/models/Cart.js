cd84dcfb487cea8dbfdaca58000e238df8c8d8c1
Koulinn <drdverzola@gmail.com>
2021-09-03 20:35:42 +0100

    Updated schema

--- src/db/models/Cart.js
+++ src/db/models/Cart.js
@@ -3,9 +3,15 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose
 
 const cartSchema = new Schema({
-    status: { type: String, enum:["active", "inTransport", "abandoned", "paid", "returned"], required: true },
-    userId: {type: Schema.Types.ObjectId, ref: "User", required: true},
-    products: [{ type: Schema.Types.ObjectId, ref: "Product", required: true }],
+    status: { type: String, enum: ["active", "inTransport", "abandoned", "paid", "returned"], required: true },
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    products: [
+        {
+            productId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+            qty: { type: Number, required: true },
+        }
+
+    ],
 }, {
     timestamps: true
 })
